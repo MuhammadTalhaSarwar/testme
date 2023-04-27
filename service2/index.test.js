b6ed94ebca452da353b3b0e897058b65826fbe80
MuhammadTalhaSarwar <61734234+MuhammadTalhaSarwar@users.noreply.github.com>
2023-04-27 16:08:28 +0500

    New f branch test (#4)
    * added service 2 tests
    
    * jest added
    
    * added missing modules
    
    * FIXED /shopping endpoint in index.test.js
    
    * FIXED /shopping endpoint in index.test.js
    
    * FIXED /shopping endpoint in index.test.js

---
+++ service2/index.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('./index');
+
+describe('GET /shopping', () => {
+  it('should return 200 OK', async () => {
+    const res = await request(app).get('/shopping');
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('should return "Hello World Container" in the response body', async () => {
+    const res = await request(app).get('/shopping');
+    expect(res.text).toContain('Hello World Container');
+  });
+});
