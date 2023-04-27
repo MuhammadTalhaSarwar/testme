27799a418803a8a43637da5200fdc854b8f29a67
MuhammadTalhaSarwar <61734234+MuhammadTalhaSarwar@users.noreply.github.com>
2023-04-27 15:47:01 +0500

    New feature (#2)
    * ADDED a index.js
    
    * ADDED s allllaaaaaaaaaaaaas in index.js
    
    * alllasssasassss2
    
    * ok done
    
    ---------
    
    Co-authored-by: Danish Manzoor <30692335+danishmanzoor@users.noreply.github.com>

--- service1/index.js
+++ service1/index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const port=3000;
 app.get('/',(req,res)=>{
-    res.send(`Hello World Container allla IDs: ${process.env.HOSTNAME}`);
+    res.send(`Hello World Container alllasssasassss2asd IDs: ${process.env.HOSTNAME}`);
 }); // get request
 const server = app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
