9f50fca5b9a485df942edc6a94c7ed8a767d2dba
unknown <talha.sarwar1989@icloud.com>
2023-04-27 16:02:47 +0500

    FIXED /shopping endpoint in index.test.js

--- service2/index.test.js
+++ service2/index.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('./index');
 
-describe('GET /', () => {
+describe('GET /shopping', () => {
   it('should return 200 OK', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
